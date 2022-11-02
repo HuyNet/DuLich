b048b97880cd33f5bd561c13c3c125611c3337f2
HuyNet <quanghuy10102001@outlook.com>
2022-11-03 01:41:38 +0700

    push giao dien login 1 nua

--- app/src/main/java/com/example/dulich/MainActivity.java
+++ app/src/main/java/com/example/dulich/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dulich;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,14 +21,14 @@ public class MainActivity extends AppCompatActivity {
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
             }
         });
 
         binding.skipBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
             }
         });
     }
